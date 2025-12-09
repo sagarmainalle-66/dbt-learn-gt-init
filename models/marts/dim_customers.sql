695ed8aa0d52a3ed31bee28aca93347e9a99e416
sagarmainalle-66 <sagar.mainalle@66degrees.com>
2025-12-09 13:07:55 +0530

    added new staging tables

--- models/marts/dim_customers.sql
+++ models/marts/dim_customers.sql
@@ -1,24 +1,16 @@
 with customers as (
 
     select
-        id as customer_id,
-        first_name,
-        last_name
-
-    from dbt-tutorial.jaffle_shop.customers
+       *
+    from {{ref('stg_jaffle_shop__customers')}}
 
 ),
 
 orders as (
 
     select
-
-        id as order_id,
-        user_id as customer_id,
-        order_date,
-        status
-
-    from dbt-tutorial.jaffle_shop.orders
+         *
+    from {{ref('stg_jaffle_shop__orders')}}
 
 ),
 
